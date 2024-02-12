1024969ab51ed7f67a24377ed12a36f5891cedd9
Jusnock <vazquezjuanfrancisco49@gmail.com>
2024-02-12 12:40:25 -0300

    Firts commit

---
+++ src/main/java/com/smallapp/singularroute/SingularRouteApplication.java
@@ -0,0 +1,13 @@
+package com.smallapp.singularroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingularRouteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingularRouteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smallapp/singularroute/SingularRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smallapp.singularroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingularRouteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
